a4ae906e72b7ca99af41b5b1b00f141d8a4eefff
Ethan Domingo <etddomin@ucsc.edu>
2022-04-02 20:40:16 -0700

    Finished section 1

---
+++ section1.py
@@ -0,0 +1,12 @@
+name = "Ethan Domingo"
+pronouns = "He/Him/His"
+favorite_movie = "My favorite movie is Perks of being a wallflower. I really enjoyed the overarching themes in the movie and has had a lasting impact on me as a person. I highly reccomend to anyone who needs a new watch."
+favorite_food = "My favorite food is Korean Barebeque. It's definitely not a meal I would eat everyday, however everytime I have it I always end up being very full. It also tastes really good after a long day."
+
+print("NAME: ", name)
+print("PRONOUNS:", pronouns)
+print("FAVORITE MOVIE:")
+print(favorite_movie)
+print("FAVORITE FOOD:")
+print(favorite_food)
+
