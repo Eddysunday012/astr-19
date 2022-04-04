6984327aa0181fc16c91981250bcd7458cd453b8
Ethan Domingo <etddomin@ucsc.edu>
2022-04-04 11:39:11 -0700

    finished session 1 and 2

---
+++ session2.py
@@ -0,0 +1,10 @@
+x = input("First floating point number: ")
+y = input("Second floating point number: ")
+
+x = float(x)
+y = float(y)
+
+print()
+print("SUM:", x+y, "TYPE:", type(x))
+print("DIFFERENCE:", x-y, "TYPE:", type(x))
+print("PRODUCT:", x*y, "TYPE:", type(x))
